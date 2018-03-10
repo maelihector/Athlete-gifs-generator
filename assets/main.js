e58b3b4694fdc43f61644f04c8ca7d549ff5b5e3
Maeli Hector <maelihector@gmail.com>
2018-03-09 18:07:09 -0600

    Fix issue of input-text submit generating more than one button per click.

--- assets/main.js
+++ assets/main.js
@@ -6,52 +6,9 @@ $(document).ready(() => {
         "Lindsey Vonn", "Antoine Griezmann", "LeBron James", "Roger Federer", "Neymar",
     ];
 
-    // Function for generating gif buttons
-    function renderButtons() {
-
-        // Delete the gifs prior to re-looping through the array
-        $("#buttons-view").empty();
-
-        // Loop through gifs array
-        for (var i = 0; i < gifs.length; i++) {
-
-            // Generate a button for each gif in the array
-            var a = $("<button>");
-            // Add a class of gif-btn to each button
-            a.addClass("gif-btn btn btn-primary");
-            // Add an attribute of data-name to each button
-            a.attr("data-name", gifs[i]);
-            // Labeling the button with the array item string
-            a.text(gifs[i]);
-            // Adding the button to the buttons-view div
-            $("#buttons-view").append(a);
-        }
-
-        $("#add-gif").on("click", function (event) {
-
-            event.preventDefault();
-
-            // Grab the input from the textbox, trim white space
-            var gif = $("#gif-input").val().trim();
-
-            // prevent submit if text-input field is empty
-            if (gif.length === 0) {
-                alert("Text-box is empty!")
-                return false
-            }
-
-            // and add/push new gif to our gifs array
-            gifs.push(gif);
-
-            // then call renderButtons() to generate new button
-            renderButtons();
-        });
-    }
-
-
     // Function to render appropriate gifs
     function displayGif() {
-
+        
         var gif = $(this).attr("data-name");
         var queryURL = "https://api.giphy.com/v1/gifs/search?q=" +
             gif + "&api_key=HxF2W3delLN8HvwXkOdecyRXMrU3FzgS&rating=g&limit=10";
@@ -89,7 +46,7 @@ $(document).ready(() => {
                 // Create a <p> to display rating
                 var p = $("<p>").text("Rating: " + rating);
 
-                // Event listener for images clicked
+                // Event listener for the qimage clicked
                 $(image).on("click", function () {
                     // Get the value of the attribute clicked and store it in var state
                     var state = $(this).attr("state");
@@ -114,20 +71,60 @@ $(document).ready(() => {
             }
         })
     }
+    
+    // Function for generating gif buttons
+    function renderButtons() {
+        
+        // Delete the gifs prior to re-looping through the array
+        $("#buttons-view").empty();
+        
+        // Loop through gifs array
+        for (var i = 0; i < gifs.length; i++) {
+            
+            // Generate a button for each gif in the array
+            var a = $("<button>");
+            // Add a class of gif-btn to each button
+            a.addClass("gif-btn btn btn-primary");
+            // Add an attribute of data-name to each button
+            a.attr("data-name", gifs[i]);
+            // Labeling the button with the array item string
+            a.text(gifs[i]);
+            // Adding the button to the buttons-view div
+            $("#buttons-view").append(a);
+        }
+    }
+
+    // Create function to handle the event for when an input value is submitted
+    // First mpty text field when user clicks in it
+    $("#gif-input").click(
+        function () {
+            $(this).val('');
+        });
+        // Function here
+    $("#add-gif").on("click", function (event) {
+
+        event.preventDefault();
+
+        // Grab the input from the textbox, trim white space
+        var gif = $("#gif-input").val().trim();
+
+        // prevent submit if text-input field is empty
+        if (gif.length === 0) {
+            alert("Text-box is empty!")
+            return false
+        }
+
+        // and add/push new gif to our gifs array
+        gifs.push(gif);
+
+        // then call renderButtons() to generate new button
+        renderButtons();
+    });
+    
+    // Call the renderButtons function to display the intial buttons
+    renderButtons();
 
     // Add a click event listener to new all elements with a class of "gif-btn" and run displayGif function
     $(document).on("click", ".gif-btn", displayGif);
 
-    // Call the renderButtons function to display the intial buttons
-    renderButtons();
-
 });
-
-// Create function to handle the event for when an input value is submitted
-
-// empty text field when user clicks on it
-
-// $("#gif-input").click(
-//     function () {
-//         $(this).val('');
-//     });
